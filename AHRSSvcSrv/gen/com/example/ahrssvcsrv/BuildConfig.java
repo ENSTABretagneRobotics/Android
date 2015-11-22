1cea2f9bcccebdf93452ebe76b0597bc15fccce1
lebarsfa <fabrice.le_bars@ensta-bretagne.fr>
2015-11-22 14:16:36 +0100

    Removed useless files

--- AHRSSvcSrv/gen/com/example/ahrssvcsrv/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.ahrssvcsrv;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- BuggyTriangle/gen/com/example/buggytriangle/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.buggytriangle;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
